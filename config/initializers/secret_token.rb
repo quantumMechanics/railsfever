acee70851db779e95005eb035dc838dd69419195
Quantum Mechanics <writewale@netscape.net>
2013-02-16 15:19:08 -0500

    initial code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quantum::Application.config.secret_token = '3c79c5d1b16f13585ff6a7dfeae72b57f6ca582790afbe1ab17c9bb6f147110e7c3ce56b9a8b6312a4f8d55f7edc2264b8410264782f2fbee492be3d570ed874'
