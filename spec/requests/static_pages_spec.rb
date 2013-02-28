c5b31c53e0c7e59f2d8bf9ce896ef019dc8bc6d9
Quantum Mechanics <writewale@netscape.net>
2013-02-27 23:15:04 -0500

    added a contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,10 +10,19 @@ describe "StaticPages" do
 		it { should have_selector("h1", class: 'second') }
 		it { should have_selector("div", class: 'landing')}
 		it { should have_selector("h2", id: 'contact-us') }
+
+		describe "link to contact" do
+			before { click_link "Contact Us Now"}
+			it_should_behave_like "contact us page"
+		end
 	end
 
 	describe "Contact us" do 
 		before { visit contact_path }
+		it_should_behave_like "contact us page"
+	end
+
+	shared_examples_for "contact us page" do 
 		it { should have_selector('form')}
 	end
 
