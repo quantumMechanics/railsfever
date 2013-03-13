e97d85e26f367633f26a3dfad3a2822313f754b2
Quantum Mechanics <writewale@netscape.net>
2013-03-13 15:45:37 -0400

    fixed bug: changed where from is defined

--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -1,10 +1,10 @@
 class ContactUsMailer < ActionMailer::Base
-  default from: "#{message.email}"
+  
   default to: ENV['MAIL_USERNAME']
 
   def new_message(message)
   	@message = message
-  	mail(subject: "#{message.subject}")
+  	mail(subject: "#{message.subject}", from:"#{message.from}" )
   end
   
 end
