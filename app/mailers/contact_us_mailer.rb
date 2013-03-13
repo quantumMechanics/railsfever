84c3aff1ed13750aa6f7dca8c7b9fae31563f143
Quantum Mechanics <writewale@netscape.net>
2013-03-13 16:08:28 -0400

    removed hardwiring of domain name

--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -1,6 +1,6 @@
 class ContactUsMailer < ActionMailer::Base
-	
-  default from: "customer@railsfever.com"
+
+  default from: "customer@#{ENV['DOMAIN']}"
   default to: ENV['MAIL_USERNAME']
 
   def new_message(message)
