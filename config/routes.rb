f4e41a5b93393de9086c2f4ab68e930e9b3ce78e
Quantum Mechanics <writewale@netscape.net>
2013-04-19 16:32:14 -0400

    added sessions and sign in

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Quantum::Application.routes.draw do
   resources :comments
   resources :blogs
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   root to: 'static_pages#home'
   #match '/contact', to: 'static_pages#contact'
