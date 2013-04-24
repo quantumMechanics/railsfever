dcc5d22d33484c8dc037f413867109fe14582d7b
Quantum Mechanics <writewale@netscape.net>
2013-04-24 11:59:59 -0400

    minor typo fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-
+	before_filter :require_signed_in_user, only: [:edit, :update]
+	before_filter :requre_correct_user, only: [:edit, :update]
+	
 	def show
 		@user = User.find(params[:id])
 	end
@@ -17,7 +19,7 @@ class UsersController < ApplicationController
 		if @user.save
 			sign_in @user
 			flash[:success] = "Welcome to Rails Fever"
-			redirect_to @user
+			redirect_to blogs_path
 		else
 			render 'new'
 		end
@@ -27,9 +29,27 @@ class UsersController < ApplicationController
 	end
 
 	def update
+		if @user.update_attributes(params[:user])
+			flash[:success] = 'Profile Updated'
+			sign_in @user
+			redirect_to blogs_path
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
 	end
+
+	private
+		
+		def require_signed_in_user
+			redirect_to signin_url, notice: 'Please Sign In.' unless signed_in?
+		end
+
+		def require_correct_user
+			@user = User.find(params[:id])
+			redirect_to(root_path) unless current_user?(@user)
+		end
 
 end
