5123098e4f396152a9d597d1e30b28cf322ed8b1
Quantum Mechanics <writewale@netscape.net>
2013-05-20 13:19:47 -0400

    tell app server to serve our pre-compiled assets

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -64,4 +64,7 @@ Quantum::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # tell app server to server our pre-compiled assets 	
+  config.serve_static_assets = true
 end
