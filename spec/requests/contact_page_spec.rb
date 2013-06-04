5baf9ab5c1957ba86d7c30b7206128e65892bd0b
Quantum Mechanics <writewale@netscape.net>
2013-06-04 17:58:15 -0400

    created stand-alone contact page test

--- spec/requests/contact_page_spec.rb
+++ spec/requests/contact_page_spec.rb
@@ -8,12 +8,13 @@ describe "ContactPage" do
 	before { visit contact_path }
 
 	describe "Contact us" do 
-		it_should_behave_like "contact us page"
-	end
-
-	describe "After clicking logo" do
-		before { click_link 'logo-not-homepage' }
-		it_should_behave_like "Home page"
+		it { should have_selector('form')}
+		it { should have_selector('form input', type: 'text', id: 'first-name') }
+		it { should have_selector('form input', type: 'text', id: 'last-name') }
+		it { should have_selector('form input', type: 'email', id: 'email') }
+		it { should have_selector('form input', type: 'tel', id: 'tel') }
+		it { should have_selector('form input', type: 'text', id: 'subject') }
+		it { should have_selector('form textarea', id: 'message') }
 	end
 	
 end
