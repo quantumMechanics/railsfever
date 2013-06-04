635f38d6abdc401389d0166aaf18122749a9f97a
Quantum Mechanics <writewale@netscape.net>
2013-06-04 17:56:06 -0400

    moved contact tests out of here

--- spec/requests/shared.rb
+++ spec/requests/shared.rb
@@ -1,12 +1,6 @@
 
 shared_examples_for "contact us page" do 
-		it { should have_selector('form')}
-		it { should have_selector('form input', type: 'text', id: 'first-name') }
-		it { should have_selector('form input', type: 'text', id: 'last-name') }
-		it { should have_selector('form input', type: 'email', id: 'email') }
-		it { should have_selector('form input', type: 'tel', id: 'tel') }
-		it { should have_selector('form input', type: 'text', id: 'subject') }
-		it { should have_selector('form textarea', id: 'message') }
+		
 end
 
 shared_examples_for "Home page" do
