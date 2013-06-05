75503c0265cafda5bcf51454036bef6da3920a3f
Quantum Mechanics <writewale@netscape.net>
2013-06-05 17:02:55 -0400

    added back jquery_ujs, signout needs this\!

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-// require jquery_ujs
+
 
 //= require jquery
+//= require jquery_ujs
 //= require fancybox_init
 //= require fancybox/source/jquery.fancybox
 //= require fancybox/source/jquery.fancybox.pack
