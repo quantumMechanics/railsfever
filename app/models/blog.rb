58c445a7715440246939d27f327cca68ffc5b20a
Quantum Mechanics <writewale@netscape.net>
2013-06-10 16:53:23 -0400

    added 2 methods

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -4,4 +4,12 @@ class Blog < ActiveRecord::Base
   validates :content, presence: true
   validates :title, presence: true
   default_scope order: 'blogs.created_at DESC'
+
+  def previous_post
+  	self.class.first(conditions: ["id < ?", id], order: "id desc")
+  end
+
+  def next_post
+  	self.class.last(conditions: ["id > ?", id], order: "id asc")
+  end
 end
