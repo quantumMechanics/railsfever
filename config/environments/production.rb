3978b6a1f6565b85b5258be9cb6393007af7ca48
Quantum Mechanics <writewale@netscape.net>
2013-06-18 17:40:16 -0400

    updated production.rb to match staging.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,9 +8,6 @@ Quantum::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
-
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
@@ -64,4 +61,10 @@ Quantum::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # tell app server to server our pre-compiled assets  
+  config.serve_static_assets = true
+
+  #include plain cs and js files
+  config.assets.precompile += %w( *.js *.css )
 end
