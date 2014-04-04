17f4815b57f04eba37490adc44c92a33345a1329
wale <writewale@gmail.com>
2014-04-03 20:06:43 -0400

    allow strikethrough, also this commit will force heroku to update ruby version

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -4,7 +4,7 @@ module BlogsHelper
 					:no_intraemphasis, :fenced_code, :gh_blockcode]
 		renderer = Redcarpet::Render::HTML.new(filter_html:true, hard_wrap:true, prettify:true)
 		markdown = Redcarpet::Markdown.new(renderer, no_intraemphasis:true, fenced_code_blocks:true, 
-				disable_indented_code_blocks:true, underline:true, tables:true)
+				disable_indented_code_blocks:true, underline:true, tables:true, strikethrough:true)
 		markdown.render(text).html_safe
 	end
 
