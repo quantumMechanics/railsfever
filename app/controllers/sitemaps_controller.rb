4aaa97f67fd78ec0660e8460ccbc931446c15966
wale <writewale@gmail.com>
2014-05-23 15:07:43 -0400

    simplified the controller

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -1,10 +1,9 @@
 class SitemapsController < ApplicationController
-	#caches_page :index
+	respond_to :xml
+
 	def index
 		@static_paths = [contact_path, about_path, blogs_path]
 		@blogs = Blog.all
-		respond_to do |format|
-			format.xml
-		end
+		#then render the view template
 	end
 end
