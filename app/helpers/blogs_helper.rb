3ae01b0454f1b5c8b3ef855b5dc3f11227412709
wale <writewale@gmail.com>
2014-06-20 17:04:25 -0400

    changed redcarpet settings

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,9 +2,10 @@ module BlogsHelper
 	def markdown(text)
 		options = [:hard_wrap, :filter_html, :autolink,
 					:no_intraemphasis, :fenced_code, :gh_blockcode]
-		renderer = Redcarpet::Render::HTML.new(filter_html:true, hard_wrap:true, prettify:true)
+		renderer = Redcarpet::Render::HTML.new(hard_wrap:true, prettify:true)
 		markdown = Redcarpet::Markdown.new(renderer, no_intraemphasis:true, fenced_code_blocks:true, 
-				disable_indented_code_blocks:true, underline:true, tables:true, strikethrough:true)
+				disable_indented_code_blocks:true, underline:true, tables:true, strikethrough:true, autolink:true,
+				lax_spacing:true, superscript:true, highlight:true, quote:true, footnotes:true)
 		markdown.render(text).html_safe
 	end
 
